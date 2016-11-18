923c518054a628aade926994ed80590782012ed9
yurahuna <yurahuna@gmail.com>
2016-11-19 08:01:02 +0900

    using namespace std;

--- NMPuzzle/tests/judge.cc
+++ NMPuzzle/tests/judge.cc
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <vector>
 
+using namespace std;
+
 #define rep(i, n) for (int i = 0; i < int(n); i++)
 #define rep2(i, a, b) for (int i = int(a); i < int(b); i++)
 
-vector<vector<int>> readIntTable(InStream& in, TResult pe, int n, int m) {
+vector<vector<int> > readIntTable(InStream& in, TResult pe, int n, int m) {
     // TODO: 出力制約を満たしているか(行列のサイズ、1..NMの数字がdistinctに出現, etc...)
-    vector<vector<int>> ret;
+    vector<vector<int> > ret;
     rep(i, n) {
         rep(j, m) {
             ret[i][j] = in.readInt();
@@ -33,7 +35,7 @@ int main(int argc, char * argv[])
     inf.readEof();
 
     // participantの出力
-    vector<vector<int>> a = readIntTable(ouf, _pe, N, M);
+    vector<vector<int> > a = readIntTable(ouf, _pe, N, M);
     // 想定出力ファイルは使わない
 
     // スコアの計算
