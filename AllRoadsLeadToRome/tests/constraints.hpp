98012a1d359534c0b13ed86e3f59b8854f59bead
torus711 <ktrumpet.zwei@gmail.com>
2017-01-04 23:48:01 +0900

    [rome] modify constraints.hpp

--- AllRoadsLeadToRome/tests/constraints.hpp
+++ AllRoadsLeadToRome/tests/constraints.hpp
@@ -1,6 +1,6 @@
-/*
-#define MIN_A 1
-#define MAX_A 1000
-#define MIN_B 1
-#define MAX_B 1000
-*/
+constexpr int MIN_N = 2;
+constexpr int MAX_N = 100000;
+constexpr int MIN_D = 1;
+constexpr int MAX_D = 1000000000;
+constexpr int MIN_C = 0;
+constexpr int MAX_C = 1000;
