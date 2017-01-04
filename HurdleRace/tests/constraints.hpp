fbdc396f886e020e09eb1ea8d9bc196b7cb7f2c3
Yazaten <Yazaten@gmail.com>
2017-01-04 15:41:03 +0900

    ハードル走の入力ケースを追加した

--- HurdleRace/tests/constraints.hpp
+++ HurdleRace/tests/constraints.hpp
@@ -1,6 +1,13 @@
-/*
-#define MIN_A 1
-#define MAX_A 1000
-#define MIN_B 1
-#define MAX_B 1000
-*/
+#define MIN_N 1
+#define MAX_N 100000
+#define MIN_L 1
+#define MAX_L ((int)(1e9))
+#define MIN_Xi 1
+#define MAX_Xi ((int)(1e9))
+
+#define MIN_N_SMALL 1
+#define MAX_N_SMALL 1000
+#define MIN_L_SMALL 1
+#define MAX_L_SMALL 100
+#define MIN_Xi_SMALL 1
+#define MAX_Xi_SMALL 1000
--- HurdleRace/tests/generator.cc
+++ HurdleRace/tests/generator.cc
@@ -1,10 +1,10 @@
-/*
 #include <iostream>
 #include <utility>
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <sys/types.h>
 #include <unistd.h>
+#include <random>
 
 using namespace std;
 
@@ -12,11 +12,14 @@ using namespace std;
 
 // aとbをファイルストリームに出力する
 // ファイル名は prefix_num.in (ex: 00_sample_00.in)
-void output(int a, int b, const string &prefix, int num){
+void output(int a, int b, vector<int> X, const string &prefix, int num){
     char name[100];
     sprintf(name, "%s_%02d.in", prefix.c_str(), num);
     ofstream ofs(name);
     ofs << a << " " << b << endl;
+    rep(i,X.size()){
+        ofs << X[i] << endl;
+    }
     ofs.close();
 }
 
@@ -25,23 +28,73 @@ int main(){
     // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
     rnd.setSeed(time(0)+getpid());
 
-    // 乱数ケースを10個生成
-    for(int i = 0; i < 10; ++i){
-        int A = rnd.next(MIN_A, MAX_A);
-        int B = rnd.next(MIN_B, MAX_B);
-        output(A, B, "50_random", i);
+    // Xiの大きいケースを10個生成
+    for(int case_num = 0; case_num < 10; ++case_num){
+        int N = rnd.next(MIN_N, MAX_N);
+        int L = rnd.next(MIN_L, MAX_L);
+        vector<int> X(N);
+        while(1){
+            for(int i=0;i<N;i++){
+                X[i] = rnd.next(MIN_Xi, MAX_Xi);
+            }
+            sort(X.begin(),X.end());
+            bool valid = true;
+            for(int j=0;j<X.size()-1;j++){
+                if(X[j]==X[j+1])valid = false;
+            }
+            if(valid)break;
+        }
+        output(N, L, X, "50_random_large", case_num);
     }
 
-    // 片方が大きいケースを生成
-    for(int i = 0; i < 10; ++i){
-        int A = 1;
-        int B = 1;
-        while(0.5*A <= B && B <= 1.5*A){
-            A = rnd.next(MIN_A, MAX_A);
-            B = rnd.next(MIN_B, MAX_B);
-        }
-        if(rnd.next(0,1)) swap(A, B);
-        output(A, B, "60_unbalance", i);
+    // Xiの小さいケースを10個生成
+    vector<int> numbers(MAX_Xi_SMALL);
+    for(int i=0;i<numbers.size()-1;i++)numbers[i]=i+1;
+    for(int case_num = 0; case_num < 10; ++case_num){
+        int N = rnd.next(MIN_N, MAX_N_SMALL);
+        int L = rnd.next(MIN_L, MAX_L);
+        shuffle(numbers.begin(),numbers.end());
+
+        vector<int> X(N);
+        for(int i=0;i<N;i++)X[i] = numbers[i];
+        sort(X.begin(),X.end());
+
+        output(N, L, X, "50_random_small", case_num);
+
     }
+
+    // Lのちいさいケースを20個生成
+    for(int case_num = 0; case_num < 20; ++case_num){
+        int N = rnd.next(MIN_N, MAX_N_SMALL);
+        int L = rnd.next(MIN_L, MAX_L_SMALL);
+        shuffle(numbers.begin(),numbers.end());
+
+        vector<int> X(N);
+        for(int i=0;i<N;i++)X[i] = numbers[i];
+        sort(X.begin(),X.end());
+
+        output(N, L, X, "50_random_Lsmall", case_num);
+
+    }
+
+    // X1<Lのケースを5個生成
+    for(int case_num = 0; case_num < 5; ++case_num){
+        int N = rnd.next(MIN_N, MAX_N);
+        int L = rnd.next(MIN_L+1, MAX_L);
+        vector<int> X(N);
+        while(1){
+            for(int i=0;i<N;i++){
+                X[i] = rnd.next(MIN_Xi,MAX_Xi);
+            }
+            sort(X.begin(),X.end());
+            bool valid = true;
+            for(int j=0;j<X.size()-1;j++){
+                if(X[j]==X[j+1])valid = false;
+            }
+            if(valid)break;
+        }
+        if(X[0]>=L)X[0] = rnd.next(1,X[0]-1);
+        output(N, L, X, "50_random_x1small", case_num);
+    }
+
 }
-*/
