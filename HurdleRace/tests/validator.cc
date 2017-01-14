8304ef2f8ba25d6a5284a11f88f40cee98b017a3
Sho Iizuka <sho.i518@gmail.com>
2017-01-15 02:20:58 +0900

    ハードル走の入力検証器を追加

--- HurdleRace/tests/validator.cc
+++ HurdleRace/tests/validator.cc
@@ -1,14 +1,21 @@
-/*
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <cassert>
+#include <vector>
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    int N = inf.readInt(MIN_N, MAX_N);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    inf.readInt(MIN_L, MAX_L);
+    inf.readEoln();
+    vector<int> X(N);
+    for (int i = 0; i < N; i++) {
+      X[i] = inf.readInt(MIN_Xi, MAX_Xi);
+      if (i > 0) {
+        assert(X[i] > X[i - 1]);
+      }
+    }
     inf.readEoln();
     inf.readEof();
 }
-*/
