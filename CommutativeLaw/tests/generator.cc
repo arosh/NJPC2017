eeb6087c7b040e965d28293280a5ca219c4ba61d
torus711 <ktrumpet.zwei@gmail.com>
2017-01-14 13:52:23 +0900

    [CommutativeLaw] tests: add fibonacci strings

--- CommutativeLaw/tests/generator.cc
+++ CommutativeLaw/tests/generator.cc
@@ -39,6 +39,27 @@ void gen_random( ostream & out, const int min_l, const int max_l, const char min
 	return;
 }
 
+void gen_fibonacci( ostream &out,
+		const int min_l, const int max_l,
+		const char a = 'a', const char b = 'b' )
+{
+	const int L = rng.next( min_l, max_l );
+
+	string S1( 1, a ), S2( 1, b ), S3;
+
+	while ( S1.size() + S2.size() <= L )
+	{
+		const string &&S = S1 + S2;
+		S1 = S2;
+		S2 = S3;
+		S3 = S;
+	}
+
+	out << S3 << endl;
+
+	return;
+}
+
 int main()
 {
 	rng.setSeed( 711 );
@@ -67,6 +88,15 @@ int main()
 		{
 			ofstream out( fname() );
 			gen_random( out, MAX_L, MAX_L, rng.next( 'a', 'z' ), 'z' );
+		}
+	}
+
+	{ // fibonacci
+		FileName fname( 4, "fibonacci" );
+		for ( int i = 0; i < 10; ++i )
+		{
+			ofstream out( fname() );
+			gen_fibonacci( out, MAX_L, MAX_L, rng.next( 'a', 'z' ), rng.next( 'a', 'z' ) );
 		}
 	}
 
