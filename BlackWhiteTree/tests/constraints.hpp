874b6caffaebe694c8b24a2da301e8a9595958a3
Yazaten <Yazaten@gmail.com>
2017-01-14 20:37:12 +0900

    白黒ツリーの制約を編集

--- BlackWhiteTree/tests/constraints.hpp
+++ BlackWhiteTree/tests/constraints.hpp
@@ -1,10 +1,13 @@
 #define MIN_N 2
-// #define MAX_N 100000
-#define MAX_N 10
+#define MAX_N 100000
+//#define MAX_N 10
+
 #define MIN_Q 1
-// #define MAX_Q 100000
-#define MAX_Q 10
+#define MAX_Q 100000
+//#define MAX_Q 10
+
 // #define MIN_Pi MIN_N
 // #define MAX_Pi MAX_N
+
 #define MIN_Ci 0
 #define MAX_Ci 1
